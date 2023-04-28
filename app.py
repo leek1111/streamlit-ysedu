bfb3b1989cb0d87b70f16dc3463a3d5be3e9a92d
lky <lgy223318@gmail.com>
2023-04-28 11:28:34 +0900

    updated

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from utils import html_temp
 from utils import dec_temp
 from eda_app import run_eda_app
 from ml_app import run_ml_app
+from stat_app import run_stat_app
 def main():
     st.markdown(html_temp, unsafe_allow_html=True)
-    menu = ['HOME', 'EDA', 'ML', 'About']
+    menu = ['HOME', 'EDA', 'ML', 'STAT','About']
     choice = st.sidebar.selectbox("Menu", menu)
     if choice == 'HOME':
         st.subheader('HOME')
@@ -15,6 +16,8 @@ def main():
         run_eda_app()
     elif choice == 'ML':
         run_ml_app()
+    elif choice == 'STAT':
+        run_stat_app()
     else:
         st.subheader('About')
 if __name__ == "__main__":
--- eda_app.py
+++ eda_app.py
@@ -24,51 +24,67 @@ def run_eda_app():
         with st.expander('기술통계량'):
             result2 =iris.describe()
             st.write(result2)
-
         with st.expander('타깃 빈도 수 확인'):
             st.write(iris['species'].value_counts())
-    elif submenu == '그래프분석':
-        st.title("Title")
-        with st.expander("산점도"):
-            fig2 = px.scatter(iris,
-                             x='sepal_width',
-                             y='sepal_length',
-                             color='species',
-                             size='petal_width',
-                             hover_data=['petal_length'])
-            st.plotly_chart(fig2)
 
-        #layouts
+    elif submenu == '그래프분석':
+        st.title('Title')
+        with st.expander('산점도'):
+            fig1 = px.scatter(iris,
+                             x = 'sepal_width',
+                             y ='sepal_length',
+                             color = 'species',
+                             size = 'petal_width',
+                             hover_data = ['petal_length'])
+            st.plotly_chart(fig1)
+
+        # layouts
         col1, col2 = st.columns(2)
         with col1:
             st.title('Seaborn')
-            fig, ax = plt.subplots()
-            sns.boxplot(iris,x='species',y='sepal_length',ax=ax)
+            fig, ax= plt.subplots()
+            sns.boxplot(iris, x ='species',y = 'sepal_length', ax = ax)
             st.pyplot(fig)
-            # 그래프 작성
 
         with col2:
             st.title('Matplotlib')
-            # 그래프작성
-            fig1, ax=plt.subplots()
-            ax.hist(iris['sepal_length'],color='green')
-            st.pyplot(fig1)
+            fig, ax = plt.subplots()
+            ax.hist(iris['sepal_length'], color="green")
+            st.pyplot(fig)
 
-        # Tabs
-        tab1, tab2 =st.tabs(['탭1','탭2'])
+        # Taps
+        tab1, tab2, tab3 = st.tabs(['탭1', '탭2', '탭3'])
         with tab1:
-            st.write("탭1")
-            if iris(iris['species'] == 'setosa'):
-                fig2 = px.scatter(iris,
-                                  x='sepal_width',
-                                  y='sepal_length')
-            # 종 선택할때 마다
-            # 산점도 그래프 가 달라지도록 함
-            # plotly 그래프로 구현
-        with tab2:
-            st.write("탭2")
-            # 캐글 데이터 /공모전데이터
-            # 해당데이터 그래프 1개만그리기
+            st.write('Setosa')
+            choice1 = iris['species'].unique()[0]
+            result3 = iris[iris['species'] == choice1]
+            fig2 = px.scatter(result3,
+                              x='sepal_width',
+                              y='sepal_length')
+            st.plotly_chart(fig2)
+
+        # with tab2:
+        #      st.write('virsicolor')
+        #      choice2 = iris['species'].unique()[1]
+        #      result4 = iris[iris['species'] == choice2]
+        #      fig3 = px.scatter(result4,
+        #                        x='sepal_width',
+        #                        y='sepal_length')
+        #     st.plotly_chart(fig3)
+        #
+        # with tab3:
+        #      st.write('virginica')
+        #      choice3 = iris['species'].unique()[2]
+        #      result5 = iris[iris['species'] == choice3]
+        #      fig4 = px.scatter(result5,
+        #                         x='sepal_width',
+        #                         y='sepal_length')
+        #     st.plotly_chart(fig4)
+
+
+            # 캐글 데이터 / 공모전 데이터
+            # 해당데이터 그래프 1개만 그리기
+
     elif submenu == '통계분석':
         pass
     else:
